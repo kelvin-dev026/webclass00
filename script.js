4e98e707c3dbe1fe58f557192408ed634d4e0d6c
kelvin-dev026 <dicksonkelvin227@gmail.com>
2025-03-12 18:10:35 +0100

    few commit

--- script.js
+++ script.js
@@ -57,3 +57,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-links a");
+    const currentPage = window.location.pathname;
+
+    navLinks.forEach(link => {
+        if (link.href.includes(currentPage)) {
+            link.classList.add("active");
+        }
+    });
+});
+
+
